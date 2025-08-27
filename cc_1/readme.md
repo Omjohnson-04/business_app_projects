4ef94ef470c31c3fec057df4959936007098f710
Owen <omjohnson@usf.edu>
2025-08-27 17:38:49 -0400

    added console log mesage, corrected spellng, and added a read me file

---
+++ cc_1/readme.md
@@ -0,0 +1,3 @@
+# this is an example
+
+this is used to make notes for those who read code
--- cc_1/script.js
+++ cc_1/script.js
@@ -1,2 +1,3 @@
 console.log("I'm hiding from you...")
 console.log(".....")
+console.log("hehe you found me")
